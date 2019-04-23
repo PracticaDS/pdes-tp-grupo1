843e357095da16b5199571f693ae4d5f57b25165
lrinaudo <leandro.rinaudo@unq.edu.ar>
2019-04-23 16:23:10 -0300

    add seller machine story

---
+++ src/components/SellerMachine.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './SellerMachine.css';
+
+export class SellerMachine extends React.Component {
+  render() {
+    return (
+      <div className='seller'></div>
+    );
+  }
+}
--- src/stories/index.js
+++ src/stories/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from '@storybook/react/demo';
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button>Hello Button</Button>
+import { SellerMachine } from '../components/SellerMachine';
+
+storiesOf('SellerMachine', module)
+  .add('Seller', () => (
+    <SellerMachine />
   ))
-  .add('with emoji', () => (
-    <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
-  ));  
+  ;
