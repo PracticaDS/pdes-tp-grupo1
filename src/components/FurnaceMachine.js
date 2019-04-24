64999d298a70bab9e1edb583e07ee57f700a0c01
hslavich <hernan.slavich@gmail.com>
2019-04-24 17:09:34 -0300

    Add FurnaceMachine. Closes #5

---
+++ src/components/FurnaceMachine.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './FurnaceMachine.css'
+
+export class FurnaceMachine extends React.Component {
+  render () {
+    return <div className='furnace' />
+  }
+}
--- src/stories/index.js
+++ src/stories/index.js
@@ -4,8 +4,10 @@ import { storiesOf } from '@storybook/react'
 import { CrafterMachine } from '../components/CrafterMachine'
 import { StarterMachine } from '../components/StarterMachine'
 import { SellerMachine } from '../components/SellerMachine'
+import { FurnaceMachine } from '../components/FurnaceMachine'
 
 storiesOf('Maquinas', module)
   .add('Crafter', () => <CrafterMachine />)
   .add('Starter', () => <StarterMachine />)
   .add('Seller', () => <SellerMachine />)
+  .add('Furnace', () => <FurnaceMachine />)
