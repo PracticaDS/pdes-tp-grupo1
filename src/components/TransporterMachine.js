7ceb8fad4a59765163e9d798cb1be8dac56655cc
hslavich <hernan.slavich@gmail.com>
2019-04-24 17:11:20 -0300

    Add TransporterMachine. Closes #6

---
+++ src/components/TransporterMachine.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './TransporterMachine.css'
+
+export class TransporterMachine extends React.Component {
+  render () {
+    return <div className='transporter' />
+  }
+}
--- src/stories/index.js
+++ src/stories/index.js
@@ -5,9 +5,11 @@ import { CrafterMachine } from '../components/CrafterMachine'
 import { StarterMachine } from '../components/StarterMachine'
 import { SellerMachine } from '../components/SellerMachine'
 import { FurnaceMachine } from '../components/FurnaceMachine'
+import { TransporterMachine } from '../components/TransporterMachine'
 
 storiesOf('Maquinas', module)
   .add('Crafter', () => <CrafterMachine />)
   .add('Starter', () => <StarterMachine />)
   .add('Seller', () => <SellerMachine />)
   .add('Furnace', () => <FurnaceMachine />)
+  .add('Transporter', () => <TransporterMachine />)
