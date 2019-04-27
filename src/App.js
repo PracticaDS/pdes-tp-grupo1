6752bfbb0b3705bbb1527edad834e4e9195965aa
hslavich <hernan.slavich@gmail.com>
2019-04-27 18:47:37 -0300

    Fix import images

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
 
-function App () {
+import { MachineToolbox} from './components/MachineToolbox'
+import { Factory } from './components/Factory';
+
+function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">
+      <MachineToolbox />
+      <Factory h={8} w={8} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
