2ab69a543db9377275f6ed0f04256480fd3025cc
Martin Ramos <marprg@gmail.com>
2019-05-05 13:05:30 -0300

    Refactor buttons of editions

---
+++ src/components/ButtonEdition.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import '../styles/ButtonEdition.css'
+
+export class ButtonEdition extends React.Component {
+  render () {
+    return <div className='buttonEditor' />
+  }
+}
