e545aa369d2b80a24d98888c2456ea18a57ab957
Martin Ramos <marprg@gmail.com>
2019-05-09 17:57:53 -0300

    Test machine rotate

--- src/test/machine.test.js
+++ src/test/machine.test.js
@@ -6,4 +6,12 @@ it('Test machine properties', () => {
   expect(machine.name).toEqual('CRAFTER')
   expect(machine.orientation).toEqual('DOWN')
   expect(machine.material).toEqual('')
-})
+});
+
+it('Test machine rotate', () => {
+  const machine = new CrafterMachine()
+  machine.orientation = 'DOWN'
+  machine.rotate()
+  expect(machine.orientation).toEqual('RIGHT')
+});
+
