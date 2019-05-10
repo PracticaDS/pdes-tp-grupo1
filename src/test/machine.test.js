5a35b3ec8e54e8883514b8dc22e7200cd736c639
Martin Ramos <marprg@gmail.com>
2019-05-10 18:31:17 -0300

    Create test of function update of StarterMachine. Closes #19

--- src/test/machine.test.js
+++ src/test/machine.test.js
@@ -1,4 +1,4 @@
-import { CrafterMachine } from '../model/machine'
+import { CrafterMachine, StarterMachine } from '../model/machine'
 
 it('Test machine properties', () => {
   const machine = new CrafterMachine()
@@ -8,10 +8,32 @@ it('Test machine properties', () => {
   expect(machine.material).toEqual('')
 });
 
+it('Test machine with id', () => {
+  const machine = new StarterMachine(2)
+
+  expect(machine.name).toEqual('STARTER')
+  expect(machine.id).toEqual(2)
+});
+
 it('Test machine rotate', () => {
   const machine = new CrafterMachine()
   machine.orientation = 'DOWN'
   machine.rotate()
   expect(machine.orientation).toEqual('RIGHT')
 });
+
+it('Starter machine: when a count of update is equals a frecuency, return a TransportUpdate wich Id is id machine + Dir', () => {
+  const machine = new StarterMachine(3)
+  machine.frecuency = 3
+  
+  machine.update();
+  machine.update();
+
+  var rta = machine.update();
+  
+  expect(rta[0].id).toEqual(11)
+});
+
+
+
 
