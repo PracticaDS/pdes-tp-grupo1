51b211f4c5680705def73b73782a102502d98004
lrrinaudo <lrrinaudo@gmail.com>
2019-05-09 22:04:20 -0300

    fix warnings

--- src/components/FactoryBlock.js
+++ src/components/FactoryBlock.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import { Machine } from './Machine'
 
 export const FactoryBlock = (props) => {
-  const renderMachine = () => props.machine ? <Machine machine={props.machine} /> : ''
+  // const renderMachine = () => props.machine ? <Machine machine={props.machine} /> : ''
 
   if (props.machine) {
     return <span className='block'><Machine onClick={() => props.onMachineClick(props.machine)} machine={props.machine} /></span>
--- src/components/Money.js
+++ src/components/Money.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-const constants = require('../constants')
-
 export const Money = ({ money }) => {
   return (
     <div className='money'>Dinero: ${money} </div>
